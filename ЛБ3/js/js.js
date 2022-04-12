b23db38018a5ddd828c57c839fc8e393ae2eb644
User <noveler243@gmail.com>
2022-04-12 19:41:47 +0300

    Old version - before flex

--- ЛБ3/js/js.js
+++ ЛБ3/js/js.js
@@ -1,8 +1,10 @@
+/*
 for (let numerator = 2; numerator < 10; ++numerator) {
     for (let denominator = 1; denominator < 10; ++denominator) {
         let res=`${numerator} x ${denominator} = ${numerator * denominator}`
         console.log(res);
-        document.write(`${res} <br>`)
+        // document.write(`${res} <br>`)
     }
 }
+*/
 
