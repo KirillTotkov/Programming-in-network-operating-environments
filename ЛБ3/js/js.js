86d18c0b8887d61645bc0e3b6ac0f7fab43be4fc
User <noveler243@gmail.com>
2022-04-12 22:49:29 +0300

    Done

--- ЛБ3/js/js.js
+++ ЛБ3/js/js.js
@@ -1,10 +1,13 @@
-/*
-for (let numerator = 2; numerator < 10; ++numerator) {
-    for (let denominator = 1; denominator < 10; ++denominator) {
-        let res=`${numerator} x ${denominator} = ${numerator * denominator}`
-        console.log(res);
-        // document.write(`${res} <br>`)
+let box_sl = document.querySelectorAll('.box')
+
+for (let i = 0; i < box_sl.length; ++i) {
+    let value_cont = document.createElement("div")
+    value_cont.className = "value_container"
+
+    for (let denominator = 2; denominator < 10; ++denominator) {
+        let res = `<p>${i + 2} x ${denominator} = ${(i + 2) * denominator}</p>`
+        value_cont.innerHTML += res
     }
+    box_sl[i].appendChild(value_cont)
 }
-*/
 
